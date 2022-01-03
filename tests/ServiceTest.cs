05bde68797c33f025ec73fa0eeb41404a0a84b4a
maisafranmendes <maisa.mendes@thoughtworks.com>
2022-01-03 18:58:54 -0300

    Add unit tests to weekend days booking and fidelidade

--- tests/ServiceTest.cs
+++ tests/ServiceTest.cs
@@ -5,16 +5,20 @@ using System;
 
 namespace hotelexercise.tests.ServiceTest
 {
-    public class ServiceTest//: IDisposable
+    public class ServiceTest
     {
         Service service;
-        string input;
+        string inputWeekDays;
+        string inputWithWeekendDays;
+        string inputFidelidade;
         Hotel hotel;
 
         public ServiceTest(){
             //given
             this.service = new Service();
-            this.input = "Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)";
+            this.inputWeekDays = "Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)";
+            this.inputWithWeekendDays = "Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)";
+            this.inputFidelidade = "Fidelidade: 25Mar2020(thur), 27Mar2020(fri), 28Mar2020(sat)";
             this.hotel = new Hotel(nome: "Parque das flores", classificacao: 3, valorDiaDeSemanaRegular: 110,
                                 valorFimDeSemanaRegular: 90, valorDiaDeSemanaFidelidade: 80, valorFimDeSemanaFidelidade: 80);
         }
@@ -22,7 +26,7 @@ namespace hotelexercise.tests.ServiceTest
         [Fact]
         public void shouldReturnABookingWhenTheUserSentValidParamenters() {
             //when 
-            Booking booking = service.getBookingByUserInput(input);
+            Booking booking = service.getBookingByUserInput(inputWeekDays);
 
             //then 
             Assert.True(booking is Booking);
@@ -32,13 +36,36 @@ namespace hotelexercise.tests.ServiceTest
 
 
        [Fact]
-        public void shouldReturnBookingValueHotel() {
+        public void shouldReturnBookingValueHotelToWeekDays() {
             //when 
-            Booking booking = service.getBookingByUserInput(input);
+            Booking booking = service.getBookingByUserInput(inputWeekDays);
             double bookingValue = service.calculateBookingHotel(booking, hotel);
 
             //then 
             Assert.Equal(bookingValue, 330);
         }
+
+        [Fact]
+        public void shouldReturnBookingValueHotelToWeekendDays() {
+            //when 
+            Booking booking = service.getBookingByUserInput(inputWithWeekendDays);
+            double bookingValue = service.calculateBookingHotel(booking, hotel);
+
+            //then 
+            Assert.Equal(bookingValue, 290);
+        }
+
+        [Fact]
+        public void shouldReturnBookingValueHotelFidelidade() {
+            //when 
+            Booking booking = service.getBookingByUserInput(inputFidelidade);
+            double bookingValue = service.calculateBookingHotel(booking, hotel);
+
+            //then 
+            Assert.Equal(bookingValue, 240);
+        }
+
+        //retornar mais barato
+        //se der empate considerar o q possui maior classificação
     }
 }
